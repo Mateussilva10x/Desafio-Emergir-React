d58ad85564fcb5d623e1343e3e072d3a47ef754d
Mateus Silva <mateussilva651@gmail.com>
2022-05-20 13:10:57 -0300

    criação do component loading

---
+++ src/components/Loading.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../assets/logo.png";
+
+const Loading = () => {
+  return (
+    <main id="main">
+      <section className="load">
+        <img src={logo} alt="" />
+        <h1>LOADING</h1>
+        <div className="lds-ring">
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Loading;
